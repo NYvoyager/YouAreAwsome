d3717b8e8d9c180cbb2d06a65840f50e965581c2
Kelvin <NYvoyager@gmail.com>
2025-01-16 09:07:46 -0500

    Initial Commit

---
+++ YouAreAwsome/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YouAreAwsome
+//
+//  Created by Kelvin Pena on 1/16/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YouAreAwsome/YouAreAwsomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwsomeApp.swift
+//  YouAreAwsome
+//
+//  Created by Kelvin Pena on 1/16/25.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwsomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
