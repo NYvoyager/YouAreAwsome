024e65bcffe6e9af1e469fe641676dd4fc728c5e
Kelvin <NYvoyager@gmail.com>
2025-06-02 18:28:13 -0400

    Conditional if statements and dependent buttons and image views

--- YouAreAwsome/ContentView.swift
+++ YouAreAwsome/ContentView.swift
@@ -9,29 +9,49 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var message = "I Am A Programmer!"
+    @State private var message = ""
+    @State private var imageString = ""
     
     var body: some View {
         
         VStack {
-    
-            Image(systemName: "swift")
+            Spacer()
+            
+            //con
+            //Image
+            Image(systemName: imageString)
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.orange)
-            Text(message)
             
+            //conditional Text
+            Text(message)
                 .font(.largeTitle)
-                .fontWeight(.heavy)
-                .foregroundStyle(.red)
-            Button("Click Me!") {
-              message = "Awesome!"
+                .fontWeight(.ultraLight)
+                .foregroundStyle(.black)
+            Spacer()
+            
+            //conditional button view
+            Button("Press Me!") {
+                let message1 = "You are Awesome!"
+                let message2 = "You are Great!"
+                let imageString1 = "hand.thumbsup"
+                let imageString2 = "sun.max.fill"
+                
+                if message == message1 {
+                    message = message2
+                    imageString = imageString2
+                } else {
+                    message = message1
+                    imageString = imageString1
+                }
             }
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
+            .tint(.orange)
         }
-        .padding()
     }
 }
-
 #Preview {
     ContentView()
 }
