a868ad4bcb35c78f568d838eec05fb58a7c3f1cc
Kelvin <NYvoyager@gmail.com>
2025-06-02 07:01:43 -0400

    After Button, Variable, State Lesson

--- YouAreAwsome/ContentView.swift
+++ YouAreAwsome/ContentView.swift
@@ -8,17 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var message = "I Am A Programmer!"
+    
     var body: some View {
+        
         VStack {
+    
             Image(systemName: "swift")
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.orange)
-            Text("I am an App Developer!")
+            Text(message)
             
                 .font(.largeTitle)
                 .fontWeight(.heavy)
                 .foregroundStyle(.red)
+            Button("Click Me!") {
+              message = "Awesome!"
+            }
         }
         .padding()
     }
